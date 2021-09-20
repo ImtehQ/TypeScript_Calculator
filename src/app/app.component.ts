c28f34b1125996529dcd93a14d1508b7d88362fd
ImtehQ <its_q@hotmail.com>
2021-09-20 15:09:34 +0200

    kak zooi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,71 +15,61 @@ export class AppComponent
   result:string = "";
   value:number = 0;
   errorMsg:string="";
+  inputs: string[] = [];
+  operators = ['*','/','+','-']; 
   constructor() {
 
    }
 
    clickEvent(type:string)
    {
-		if(type == 'c')
-		{
-			this.Clear();
-        }
-		else if(type == 'x')
-        {
-			this.SetInputTo(this.input.substring(0, this.input.length -1))
-        }
-        else if(type == '=')
-        {
-			this.SetResultTo(eval(this.input))
-        }
-		else if (type == '.')
-		{
-			if(this.input.length == 0)
-			{
-				this.AddToInput('0' + type)
-			}
-			if(this.input.includes('.') == false)
-			{
-				this.AddToInput(type)
-			}
-		}
-		else if (type == '0')
-		{
-			if(this.input.length == 1)
-			{
-				if(this.input[0] == '0'){}
-				else{
-					this.AddToInput(type)
-				}
-			}
-
-			if(this.input.length == 0)
-			{
-				this.AddToInput(type)
-			}
-			if(this.input.length > 1)
-			{
-				this.AddToInput(type)
-			}
-		}
-        else
-        {
-        	this.AddToInput(type)
-        }
-		if(this.input.includes('/0'))
-		{
-			this.SetErrorMsgTo('Cannot divide by zero')
-		}
-		else
-		{
-			this.SetErrorMsgTo('')
-		}
+		if(type == 'c'){this.Clear();}
+		else if(type == 'x'){this.SetInputTo(this.input.substring(0, this.input.length -1))}
+		else if(type == '='){this.CalculateResult()}
+		else{this.AddToInput(type)}
     }
+
 
 	AddToInput(t:string)
 	{
-		this.input += t;
+		if(t == '.' && this.input.slice(-1) == '.')
+		{
+			return
+		}
+		else if(t == '.' && this.input.substring(this.input.length - 1, 1) == '.')
+		{
+			return
+		}
+		else if(t == '0' && this.input.length == 0)
+		{
+			this.input += t + '.';
+			return
+		}
+		else if(t == '.' && this.input.length == 0)
+		{
+			this.input += '0' + t;
+			return
+		}
+		else if(t == '0' && this.input.length == 1 && this.input.substring(this.input.length-1) == '0')
+		{
+			return
+		}
+		else if(t == '0' && this.input.length > 1 && this.input.substring(this.input.length-1) == '/')
+		{
+			return
+		}
+		else if(t == '0' && this.input.length > 2 && this.input.substring(this.input.length-1) == '/')
+		{
+			return
+		}
+		else
+		{
+			this.input += t;
+		}
+
+
+
+
 	}
 
 	SetInputTo(t:string)
@@ -90,6 +80,7 @@ export class AppComponent
 	SetResultTo(t:string)
 	{
 		this.result = t;
+		
 	}
 
 	SetErrorMsgTo(t:string)
@@ -101,5 +92,10 @@ export class AppComponent
 	{
 		this.input = "";
 		this.result = "";
+	}
+
+	CalculateResult()
+	{
+		this.SetResultTo(eval(this.input))
 	}
 }
